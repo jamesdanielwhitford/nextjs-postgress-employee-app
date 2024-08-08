eb9bcffa1407e93c8bba0594051ccb5386ffd087
jamesdanielwhitford <70632508+jamesdanielwhitford@users.noreply.github.com>
2024-08-08 12:25:26 +0200

    Updated index.js for Vercel serverless functions

--- pages/api/employees/index.js
+++ pages/api/employees/index.js
@@ -7,24 +7,11 @@ import {
 const handler = nc();
 
 handler.get(async (req, res) => {
-  try {
-    const employees = await getAllEmployees();
-    res.status(200).json(employees);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
+  await getAllEmployees(req, res);
 });
 
 handler.post(async (req, res) => {
-  try {
-    const test = req.body;
-    console.log("input:", test);
-    const employee = await createEmployee(req.body);
-    console.log("returned:", employee);
-    res.status(201).json(employee);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
+  await createEmployee(req, res);
 });
 
 export default handler;
