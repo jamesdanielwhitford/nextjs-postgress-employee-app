9ac2a2cbca85f17e4197a31fd78a1ac4b6fbf8bc
jamesdanielwhitford <70632508+jamesdanielwhitford@users.noreply.github.com>
2024-08-08 12:08:12 +0200

    Update pages/api/employees/index.js

--- pages/api/employees/index.js
+++ pages/api/employees/index.js
@@ -2,14 +2,13 @@ import nc from 'next-connect';
 import {
   createEmployee,
   getAllEmployees,
-  
 } from '../../../backend/controllers/employeeController';
+
 const handler = nc();
 
 handler.get(async (req, res) => {
   try {
-    
-    const employees = await getAllEmployees(req,res);
+    const employees = await getAllEmployees();
     res.status(200).json(employees);
   } catch (error) {
     res.status(500).json({ error: error.message });
@@ -19,13 +18,13 @@ handler.get(async (req, res) => {
 handler.post(async (req, res) => {
   try {
     const test = req.body;
-    console.log("input:",test);
-    const employee = await createEmployee(req,res);
-    console.log("returned:",employee.body);
+    console.log("input:", test);
+    const employee = await createEmployee(req.body);
+    console.log("returned:", employee);
     res.status(201).json(employee);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
-  });
+});
 
 export default handler;
