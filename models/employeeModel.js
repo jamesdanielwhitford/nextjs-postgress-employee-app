21abd4452fb6b49ed6f2b9e170e5c3a2af6b443d
jamesdanielwhitford <70632508+jamesdanielwhitford@users.noreply.github.com>
2024-08-08 08:12:30 +0200

    Employee model Update

--- models/employeeModel.js
+++ models/employeeModel.js
@@ -1,24 +1,23 @@
-const pool = require('../config/database');
+const db = require('../config/database');
 
 const createEmployee = async (employee) => {
   const { name, email, phone } = employee;
   const query = 'INSERT INTO employees (name, email, phone) VALUES ($1, $2, $3) RETURNING *';
   const values = [name, email, phone];
-  const result = await pool.query(query, values);
+  const result = await db.query(query, values);
   return result.rows[0];
 };
 
 const getAllEmployees = async () => {
-  
   const query = 'SELECT * FROM employees';
-  const result = await pool.query(query);
+  const result = await db.query(query);
   return result.rows;
 };
 
 const getEmployeeById = async (id) => {
   const query = 'SELECT * FROM employees WHERE id = $1';
   const values = [id];
-  const result = await pool.query(query, values);
+  const result = await db.query(query, values);
   return result.rows[0];
 };
 
@@ -26,14 +25,14 @@ const updateEmployee = async (id, employee) => {
   const { name, email, phone } = employee;
   const query = 'UPDATE employees SET name = $1, email = $2, phone = $3 WHERE id = $4 RETURNING *';
   const values = [name, email, phone, id];
-  const result = await pool.query(query, values);
+  const result = await db.query(query, values);
   return result.rows[0];
 };
 
 const deleteEmployee = async (id) => {
   const query = 'DELETE FROM employees WHERE id = $1 RETURNING *';
   const values = [id];
-  const result = await pool.query(query, values);
+  const result = await db.query(query, values);
   return result.rows[0];
 };
 
