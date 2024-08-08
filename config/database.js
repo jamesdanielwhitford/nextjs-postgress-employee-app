87ebf89d64a8fb67aeeb77e13e50a6627b1d8377
jamesdanielwhitford <70632508+jamesdanielwhitford@users.noreply.github.com>
2024-08-08 08:18:12 +0200

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,32 +1,3 @@
-const { Pool } = require('@vercel/postgres');
+import { sql } from '@vercel/postgres';
 
-let pool;
-
-async function getPool() {
-  if (!pool) {
-    pool = new Pool({
-      connectionString: process.env.POSTGRES_URL,
-      ssl: {
-        rejectUnauthorized: false
-      }
-    });
-    
-    // Test the connection
-    try {
-      const client = await pool.connect();
-      console.log('Connected to database successfully');
-      client.release();
-    } catch (err) {
-      console.error('Error connecting to database:', err);
-      throw err;
-    }
-  }
-  return pool;
-}
-
-module.exports = {
-  query: async (text, params) => {
-    const pool = await getPool();
-    return pool.query(text, params);
-  },
-};
+export { sql };
